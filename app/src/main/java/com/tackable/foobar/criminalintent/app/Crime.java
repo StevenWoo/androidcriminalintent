28f830be1961612bddcc3ef020ef36d146790afb
Steven Woo <swoo@tackable.com>
2014-04-24 00:25:11 -0700

    work in progress chapter 8

--- app/src/main/java/com/tackable/foobar/criminalintent/app/Crime.java
+++ app/src/main/java/com/tackable/foobar/criminalintent/app/Crime.java
@@ -1,5 +1,6 @@
 package com.tackable.foobar.criminalintent.app;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,8 +9,13 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+
     public Crime(){
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -22,6 +28,22 @@ public class Crime {
 
     public void setTitle(String mTitle) {
         this.mTitle = mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
     }
 }
 
