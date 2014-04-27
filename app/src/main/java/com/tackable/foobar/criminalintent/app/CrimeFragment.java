c5dbd227e7af18f3ea89ed45f52d5aa7fd290d6f
Steven Woo <swoo@tackable.com>
2014-04-27 00:01:38 -0700

    start using arguments for fragment

--- app/src/main/java/com/tackable/foobar/criminalintent/app/CrimeFragment.java
+++ app/src/main/java/com/tackable/foobar/criminalintent/app/CrimeFragment.java
@@ -16,21 +16,32 @@ import android.widget.EditText;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by stevenwoo on 4/23/14.
  */
 public class CrimeFragment extends Fragment{
 
+    public static final String EXTRA_CRIME_ID = "com.tackable.foobar.criminalintent.crime_id";
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
 
+    public static CrimeFragment newInstance(UUID crimeId){
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_CRIME_ID, crimeId);
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
+        UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
     @Override
@@ -38,7 +49,7 @@ public class CrimeFragment extends Fragment{
 
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
-
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -59,12 +70,14 @@ public class CrimeFragment extends Fragment{
 
         mDateButton = (Button)v.findViewById(R.id.crime_date);
 
+
         android.text.format.DateFormat df = new android.text.format.DateFormat();
 
         mDateButton.setText(df.format("EEEE MMMM dd, yyyy hh:mm:ss",  mCrime.getDate()));
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
--- app/src/main/java/com/tackable/foobar/criminalintent/app/CrimeListFragment.java
+++ app/src/main/java/com/tackable/foobar/criminalintent/app/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.tackable.foobar.criminalintent.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
@@ -35,6 +36,10 @@ public class CrimeListFragment extends ListFragment {
         //Crime c = (Crime)(getListAdapter()).getItem(position);
         Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
         Log.d(TAG, c.getTitle() + "was clicked");
+
+        Intent i = new Intent(getActivity(), CrimeActivity.class);
+        i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
+        startActivity(i);
     }
 
     private class CrimeAdapter extends ArrayAdapter<Crime> {
